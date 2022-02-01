7441f549cdb91a2de5f2792e61e3d82ad249b172
Philip <philipbdev@gmail.com>
2022-02-01 18:08:20 -0500

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>VGNews</title>
+        <link rel="icon" href="https://i.imgur.com/flkZM59.png" />
+        <meta property="og:title" content="VGNews" />
+        <meta property="og:image" content="https://i.imgur.com/flkZM59.png" />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/sources.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const sources = () => {
+  return <div></div>;
+};
+
+export default sources;
